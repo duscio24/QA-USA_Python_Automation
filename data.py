112ccc69162c07c9df0f8ce17126134a721e836d
Phillip <duscio18@gmail.com>
2025-08-19 19:59:53 -0400

    Helpers

---
+++ data.py
@@ -0,0 +1,7 @@
+URBAN_ROUTES_URL = 'https://cnt-b7d65408-9194-41d1-9447-6256e5fde710.containerhub.tripleten-services.com'
+ADDRESS_FROM = 	'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = 	'+1 123 123 12 12'
+CARD_NUMBER = 	'1234 5678 9100'
+CARD_CODE = 	'1111'
+MESSAGE_FOR_DRIVER = 	'Stop at the juice bar, please'
